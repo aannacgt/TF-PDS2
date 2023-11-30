df4ba9f7529bce70abc977374fcb01b4d231dbf6
LucasAraujoUFMG <148714205+LucasAraujoUFMG@users.noreply.github.com>
2023-11-30 19:58:52 -0300

    Turma.cpp

--- códigos/Turma.cpp
+++ códigos/Turma.cpp
@@ -9,3 +9,14 @@ Turma::Turma(const std::string& codigo, int numeroVagas, const std::string& prof
 std::string Turma::getCodigo() const { return codigo; }
 int Turma::getNumeroVagas() const { return numeroVagas; }
 std::string Turma::getProfessor() const { return professor; }
+
+int main() {
+    Turma minhaTurma;
+
+    // Criar turmas com códigos únicos
+    minhaTurma.criarTurma("T001", 30, "Prof. A");
+    minhaTurma.criarTurma("T002", 25, "Prof. B");
+    minhaTurma.criarTurma("T001", 40, "Prof. C");  // Código duplicado, não será criada
+
+    return 0;
+}
