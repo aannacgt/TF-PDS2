4ab30b4ee3d12595e301df930afd230bad8edbce
LucasAraujoUFMG <148714205+LucasAraujoUFMG@users.noreply.github.com>
2023-11-30 19:58:15 -0300

    Turma.hpp

--- códigos/Turma.hpp
+++ códigos/Turma.hpp
@@ -3,7 +3,7 @@
 #ifndef TURMA_HPP
 #define TURMA_HPP
 
-#include <string>
+#include <vector>
 
 class Turma {
 public:
@@ -14,10 +14,44 @@ public:
     int getNumeroVagas() const;
     std::string getProfessor() const;
 
+    // Função para criar uma turma com código único
+    void criarTurma(const std::string& codigo, int numeroVagas, const std::string& professor);
+
 private:
     std::string codigo;
     int numeroVagas;
     std::string professor;
+    std::vector<Turma> turmas;
 };
+
+Turma::Turma(const std::string& codigo, int numeroVagas, const std::string& professor)
+    : codigo(codigo), numeroVagas(numeroVagas), professor(professor) {}
+
+std::string Turma::getCodigo() const {
+    return codigo;
+}
+
+int Turma::getNumeroVagas() const {
+    return numeroVagas;
+}
+
+std::string Turma::getProfessor() const {
+    return professor;
+}
+
+void Turma::criarTurma(const std::string& codigo, int numeroVagas, const std::string& professor) {
+    // Verificar se o código já foi utilizado
+    for (const auto& turma : turmas) {
+        if (turma.getCodigo() == codigo) {
+            // Código duplicado, não criar a turma
+            return;
+        }
+    }
+
+    // Código único, criar a turma
+    Turma novaTurma(codigo, numeroVagas, professor);
+    turmas.push_back(novaTurma);
+}
+
 
 #endif // TURMA_HPP
